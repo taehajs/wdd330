cd9deac1bd640f4c4ac7a46bf3ec7e2e8eb5ff11
taehajs <taehajs@naver.com>
2026-01-10 14:56:33 +0900

    p

--- src/js/product.js
+++ src/js/product.js
@@ -4,15 +4,18 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+
+  let cart = JSON.parse(localStorage.getItem("so-cart")) || [];
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
-// add to cart button event handler
+
+
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);

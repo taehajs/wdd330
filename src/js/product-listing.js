87878fe2a828097046dafa0eada5ce860a56c031
taehajs <taehajs@naver.com>
2026-01-28 19:57:34 +0900

    asdfasdf

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,17 +1,23 @@
 import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.js';
 import { loadHeaderFooter, getParam } from './utils.mjs';
 
 loadHeaderFooter();
 
 const category = getParam('category');
-
 const dataSource = new ProductData();
+const productListElement = document.querySelector('.product-list');
 
-const listElement = document.querySelector('.product-list');
+async function renderList() {
+  const products = await dataSource.getData(category);
+  productListElement.innerHTML = products.map(product => `
+    <div class="product-card">
+      <a href="../product_pages/${product.Id}.html">
+        <img src="${product.Images.PrimaryMedium}" alt="${product.Name}" />
+        <h2>${product.Name}</h2>
+        <p>${product.ListPrice}</p>
+      </a>
+    </div>
+  `).join('');
+}
 
-const myList = new ProductList(category, dataSource, listElement);
-
-myList.init();
-
-document.querySelector('h1').textContent = `Top Products: ${category}`;
+renderList();

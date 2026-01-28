72977f21955e5fb99666eb64bb87756093ef4ee2
taehajs <taehajs@naver.com>
2026-01-28 20:09:56 +0900

    asdfasdfasdfasdf

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -8,16 +8,21 @@ const dataSource = new ProductData();
 const productListElement = document.querySelector('.product-list');
 
 async function renderList() {
-  const products = await dataSource.getData(category);
-  productListElement.innerHTML = products.map(product => `
-    <div class="product-card">
-      <a href="../product_pages/${product.Id}.html">
-        <img src="${product.Images.PrimaryMedium}" alt="${product.Name}" />
-        <h2>${product.Name}</h2>
-        <p>${product.ListPrice}</p>
-      </a>
-    </div>
-  `).join('');
+  try {
+    const products = await dataSource.getData(category);
+    productListElement.innerHTML = products.map(product => `
+      <div class="product-card">
+        <a href="../product_pages/${product.Id}.html">
+          <img src="${product.Images.PrimaryMedium}" alt="${product.Name}" />
+          <h2>${product.Name}</h2>
+          <p>${product.ListPrice}</p>
+        </a>
+      </div>
+    `).join('');
+  } catch (err) {
+    console.error("Error loading products:", err);
+    productListElement.innerHTML = "<p>상품을 불러올 수 없습니다.</p>";
+  }
 }
 
 renderList();

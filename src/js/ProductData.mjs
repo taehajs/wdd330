307a024738acdf5c49af456607f3401e4a76d963
taehajs <taehajs@naver.com>
2026-01-28 20:29:06 +0900

    asdfasdf

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,23 +1,15 @@
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
-}
-
 const baseURL = import.meta.env.VITE_SERVER_URL;
 
 export default class ProductData {
   async getData(category) {
     const response = await fetch(`${baseURL}products/search/${category}`);
-    const data = await convertToJson(response);
+    const data = await response.json();
     return data.Result;
   }
 
   async findProductById(id) {
     const response = await fetch(`${baseURL}product/${id}`);
-    const data = await convertToJson(response);
+    const data = await response.json();
     return data.Result;
   }
 }

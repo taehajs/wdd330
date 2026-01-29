345fb0e32dd0424d3e1d83e2666088b0f74b6edf
taehajs <taehajs@naver.com>
2026-01-29 16:53:55 +0900

    asdf

--- src/js/product.js
+++ src/js/product.js
@@ -1,19 +1,13 @@
-import ProductData from './ProductData.mjs';
 import { getParam, loadHeaderFooter } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
 loadHeaderFooter();
 
-const productId = getParam('id');
-const dataSource = new ProductData();
+const productId = getParam('product');
 
-async function renderProductDetail() {
-  const product = await dataSource.findProductById(productId);
-  document.querySelector('.product-detail').innerHTML = `
-    <h2>${product.Name}</h2>
-    <img src="${product.Images.PrimaryLarge}" alt="${product.Name}" />
-    <p>${product.DescriptionHtmlSection}</p>
-    <p>Price: ${product.SuggestedRetailPrice}</p>
-  `;
-}
+const dataSource = new ProductData('tents');
 
-renderProductDetail();
+const product = new ProductDetails(productId, dataSource);
+
+product.init();

3c6a2bdb973ff931a0ad1549102f8747bb5124f8
taehajs <taehajs@naver.com>
2026-01-29 17:22:52 +0900

    asdf

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'src', 
+  build: {
+    outDir: '../dist', 
+    emptyOutDir: true, 
+  },
   server: {
     proxy: {
       '/products': {
